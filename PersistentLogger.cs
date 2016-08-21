d42d1f86c7c60a7e0e2a6d26eda478391d43dc8c
Leejay Schmidt <leejayschmidt@leejays-mbp.gateway.pace.com>
2016-08-21 00:03:30 -0600

    Added some comments so future readers of the code are not baffled by the happenings within

--- PersistentLogger.cs
+++ PersistentLogger.cs
@@ -29,16 +29,21 @@ namespace PersistentLogger {
     
     public static class PLog {
         
+        // the directory in which to save the log file
         public static string LogDirectory { get; set; }
+
+        // the actual file path of the log file (without directory)
         public static string LogPath { get; set; }
 
         static PLog() {
-            //set default log directory to personal folder
+            // set default log directory to personal folder
             LogDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/logging";
-            //set default log path
+            // set default log path
             LogPath = "log.txt";
         }
-
+        
+        /// <summary>
+        /// Appends a log message marked "I" into the log file specified.
         public static void Info(string tag, string message) {
             LogMessage(tag, message, "I");
         }
@@ -60,57 +65,86 @@ namespace PersistentLogger {
         }
         
         static void LogMessage(string tag, string message, string leadingChar) {
+            // get the string with the date and time details to timestamp the log message
             string dateString = DateTime.Now.ToString("yyyy/MM/dd H:mm:ss.fff");
+            // format the message string (format is TIMESTAMP LC/TAG   MESSAGE)
             string logString = dateString + "\t" + leadingChar + "/" + tag + "\t" + message;
+            // pass the message off to the log writing manager to handle from here
             LogWritingManager.QueueLog(logString, LogDirectory, LogPath);
         }
 
     }
 
     static class LogWritingManager {
+
+        // used to buffer messages until file writer is able to deal with them
         static Queue _queue;
+        
+        // used to prevent concurrent writes to file 
         static readonly object _writeLock;
-        //if a timeout occurs in writing logs out, determine if exception will be thrown
+
+        // if a timeout occurs in writing logs out, determine if exception will be thrown
         public static bool ThrowIfTimeout { get; set; }
-        //determines the timeout period for waiting on the available writer
+
+        // determines the timeout period for waiting on the available writer
         public static int TimeoutPeriod { get; set; }
-        //determine if the message should be skipped or a retry should occur post-timeout
-        //if a throw is not happening
+
+        // determine if the message should be skipped or a retry should occur post-timeout
+        // if a throw is not happening
         public static bool RetryIfTimeout { get; set; }
 
         static LogWritingManager() {
             _queue = new Queue();
             _writeLock = new object();
+            // use a default timeout of one second to prevent total hangs of the application
             TimeoutPeriod = 1000;
+            // by default, if the file writer wait times out, just drop that message and move
+            // on, in order to preven the queue from piling up
             RetryIfTimeout = false;
         }
+
         public static void QueueLog(string logString, string writeDir, string writePath) {
-            //guard from null strings
+            // guard from null strings
             if (logString == null || writeDir == null || writePath == null) {
                 return;
             }
+            // in order to prevent concurrent reads/writes from the queue, lock it while adding
             lock (_queue) {
+                // queue up the log message string
                 _queue.Enqueue(logString);
             }
+            // add the write to the thread pool. Since the messages were queued in a synchronised
+            // way, it won't matter what order they are executed in (except if the write directory
+            // or write path changed)
             ThreadPool.QueueUserWorkItem(o => WriteLog(writeDir, writePath));
         }
+
         static string DequeueLog() {
+            // this is the log message that will get returned
+            // must be declared outside of the try/catch and lock for visibility
             string log;
+            // lock on the queue to prevent concurrent reads and writes from it
             lock (_queue) {
                 try {
+                    // try to get a string from the queue. If this is not possible, 
+                    // and exception will be thrown
                     log = (string)_queue.Dequeue();
                 } catch {
+                    // in case retrieving the log message failed, log this message instead
                     log = "Error Getting Log Message";
                 }
             }
+            // return the next available log message (if applicable)
             return log;
         }
+
         static void WriteLog(string writeDir, string writePath) {
-            //wait on the write lock for 1 second then time out
-            //this will ensure that the logger does not hang on
-            //a bad write
+            // wait on the write lock for 1 second then time out
+            // this will ensure that the logger does not hang on
+            // a bad write
             if (!Monitor.TryEnter(_writeLock, TimeoutPeriod)) {
                 if (ThrowIfTimeout) {
+                    // if the user chose to throw on a timeout, then do this
                     throw new TimeoutException("Failed to write log message");
                 } else {
                     //queue a retry. This will free up this thread and try
@@ -124,7 +158,9 @@ namespace PersistentLogger {
                     }
                 }
             } else {
+                // ensure that the file manager issues will be caught
                 try {
+                    // get the next message from the queue
                     string logMessage = DequeueLog();
                     if (logMessage != null) {
                         if (!Directory.Exists(writeDir)) {
@@ -142,6 +178,7 @@ namespace PersistentLogger {
                         }
                     }
                 } finally {
+                    // ensure that the monitor always exits, even on error
                     Monitor.Exit(_writeLock);
                 }
             }
